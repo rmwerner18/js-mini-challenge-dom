d0eccdb0a4647c4d5880acfced8a0f77f68db053
humorous-storage-7521 <rmwerner@umich.edu>
2020-08-25 21:13:06 -0500

    finish

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 /***** Deliverable 1 *****/
-const header = document.querySelector("#header")
-console.log("Here's your header:", header)
+document.addEventListener("DOMContentLoaded", function() {
+    const header = document.querySelector("#header")
+    console.log("Here's your header:", header)
+    header.style.color = "red"
+    
+    
+    /***** Deliverable 2 *****/
+    
+    /***** Deliverable 3 *****/
+    function createPlayerDiv(player) {
+        let playerElement = document.createElement("div")
+        playerElement.className = "player"
+        playerElement.setAttribute('data-number', `${player.number}`)
+        
+        let playerH = document.createElement("h3")
+        playerH.innerHTML = `${player.name} <em>${player.nickname}</em>`
+        
+        playerElement.append(playerH)
 
+        return playerElement
+    }
+    
+    console.log("PLAYERS array looks like this:", PLAYERS)
 
-/***** Deliverable 2 *****/
+    let container = document.querySelector(".player-container")
 
+    PLAYERS.forEach(function(player) {
+        let playerDiv = createPlayerDiv(player)
+        container.append(playerDiv)
+    })
+    
+    
+    /***** Deliverable 4 *****/
+    let bad = document.querySelector('div[data-number="7"]')
+    bad.remove()
+})
 
-/***** Deliverable 3 *****/
-// console.log("PLAYERS array looks like this:", PLAYERS)
-
-
-/***** Deliverable 4 *****/
